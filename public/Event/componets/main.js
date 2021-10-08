bcbf22fd6e9e41069cdd9174cda9101fbe59faa0
DF_Tv <61697853+DRTFTv@users.noreply.github.com>
2021-10-08 04:40:26 -0300

    Update

--- public/Event/componets/main.js
+++ public/Event/componets/main.js
@@ -2,7 +2,7 @@ function confirming() {
     var name = prompt("Digite seu nome: ");
 
     if (name == null || name == "") {
-        alert("Nome não informado");
+        alert("Nome não informado!");
     } else {
         window.location.href = `https://api.whatsapp.com/send?phone=5513988785490&text=Eu%20${name}%20confirmo%20minha%20presen%C3%A7a%20na%20Halloween%20Party!`;
     }
