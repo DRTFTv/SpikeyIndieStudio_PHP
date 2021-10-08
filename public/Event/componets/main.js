3d19a422c9444b74a601b09c1f81cdd57e81161a
DF_Tv <61697853+DRTFTv@users.noreply.github.com>
2021-10-08 05:19:02 -0300

    Update

--- public/Event/componets/main.js
+++ public/Event/componets/main.js
@@ -4,6 +4,6 @@ function confirming() {
     if (name == null || name == "") {
         alert("Nome não informado!");
     } else {
-        window.location.href = `https://api.whatsapp.com/send?phone=5513988785490&text=Eu%20${name}%20confirmo%20minha%20presen%C3%A7a%20na%20Halloween%20Party!`;
+        window.location.href = `https://api.whatsapp.com/send?phone=5513996626206&text=Eu%20${name}%20confirmo%20minha%20presen%C3%A7a%20na%20Halloween%20Party!`;
     }
 }
