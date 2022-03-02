029c5d9358b8bf2445649b2b99820e7124bec686
Daniel Filho - DF_Tv <canaldetroladores@gmail.com>
2022-03-02 14:46:24 -0300

    Update

--- app/Views/danielfilho.php
+++ app/Views/danielfilho.php
@@ -54,23 +54,39 @@
                         </label><br>
 
                         <div>
-                            <img class="card" src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E">
-                            <img class="card" src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white">
-                            <img class="card" src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white">
+                            <img class="card" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+                            <img class="card" src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white">
+                            <img class="card" src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
+                            <img class="card" src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white">
                             <img class="card" src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white">
                             <img class="card" src="https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white">
+                            <img class="card" src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white">
+                            <img class="card" src="https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white">
                             <img class="card" src="https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white">
-                        </div><br>
+                        </div>
+                        
+                        <br>
 
+                        <div>
+                            <img class="card" src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB">
+                            <img class="card" src="https://img.shields.io/badge/React_Native-20232A?style=for-the-badge&logo=react&logoColor=61DAFB">
+                            <img class="card" src="https://img.shields.io/badge/Electron-2B2E3A?style=for-the-badge&logo=electron&logoColor=9FEAF9">
+                            <img class="card" src="https://img.shields.io/badge/Expo-1B1F23?style=for-the-badge&logo=expo&logoColor=white">
+                        </div>
+                        
+                        <br>
+                        
                         <div>
                             <img class="card" src="https://img.shields.io/badge/GODOT-%23FFFFFF.svg?style=for-the-badge&logo=godot-engine">  
                             <img class="card" src="https://img.shields.io/badge/unity-%23000000.svg?style=for-the-badge&logo=unity&logoColor=white">  
                             <img class="card" src="https://img.shields.io/badge/unrealengine-%23313131.svg?style=for-the-badge&logo=unrealengine&logoColor=white">
-                        </div><br>
+                        </div>
+                        
+                        <br>
 
                         <div>
                             <img class="card" src="https://img.shields.io/badge/adobeillustrator-%23FF9A00.svg?style=for-the-badge&logo=adobeillustrator&logoColor=white">
-                            <img class="card" src="https://img.shields.io/badge/-Corel%20Draw-%2361a804">
+                            <img class="card" height="25em" src="https://img.shields.io/badge/-Corel%20Draw-%2361a804">
                             <img class="card" src="https://img.shields.io/badge/Adobe%20Photoshop-31A8FF?style=for-the-badge&logo=Adobe%20Photoshop&logoColor=black">
                             <img class="card" src="https://img.shields.io/badge/Adobe%20XD-470137?style=for-the-badge&logo=Adobe%20XD&logoColor=#FF61F6">
                             <img class="card" src="https://img.shields.io/badge/blender-%23F5792A.svg?style=for-the-badge&logo=blender&logoColor=white">
